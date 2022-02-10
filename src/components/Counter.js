e054b540fbc0eb8e4c8eb3efa5527a7984404d71
andrey12v <andrey12v@gmail.com>
2022-02-09 21:05:11 -0600

    Add Redux Provider and use the hook useSelector to get data from Redux
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25600168#overview

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,18 @@
+import { useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  //useSelector is redux hook we use to  get redux data
+  //we pass redux function to it to get counter from the state
+  const counter = useSelector(state => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
