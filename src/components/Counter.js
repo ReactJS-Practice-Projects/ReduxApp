c0a5514da5d3cc6432d952dfe5b00497a4cc7a08
andrey12v <andrey12v@gmail.com>
2022-02-09 22:05:31 -0600

    Implement Redux with Class based component
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25600172#overview

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,10 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, connect } from 'react-redux';
 
 import classes from './Counter.module.css';
 
+/*
 const Counter = () => {
-  //add useDispatch hook to dispatch redux actions
+   //add useDispatch hook to dispatch redux actions
   const dispatch = useDispatch();
   
   //useSelector is redux hook we use to  get redux data
@@ -34,3 +35,58 @@ const Counter = () => {
 };
 
 export default Counter;
+*/
+
+
+import { Component } from 'react';
+
+class Counter extends Component {
+  incrementHandler() {
+    this.props.increment();
+  }
+
+  decrementHandler() {
+    this.props.decrement();
+  }
+
+  toggleCounterHandler() {}
+
+  //this.incrementHandler.bind(this) - we do bind(this) to make sure that these menthods reffer to this class 
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+        </div>
+        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
+
+//here we pick counter form the state and bound it to the counter variable 
+//it is equivalent to the hook in the functional component 
+//const counter = useSelector(state => state.counter); 
+//here the state is the argument in the function
+const mapStateToProps = state => {
+  return {
+    counter: state.counter
+  };
+}
+
+//this is equivalent to the functional dispatch function
+//const dispatch = useDispatch();
+//here the dispatch is the argument in the function 
+const mapDispatchToProps = dispatch => {
+  return {
+    increment: () => dispatch({ type: 'increment' }),
+    decrement: () => dispatch({ type: 'decrement' }),
+  }
+};
+
+//here we pass our class counter, and two other parametsr are the state and actions
+//mapStateToProps, and mapDispatchToProps are pointers that are executed by redux
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
